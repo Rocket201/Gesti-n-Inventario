fdeea79d933bbc4866945538df8a29a33ce7486b
Rocket201 <alejandrocorroto99@gmail.com>
2023-10-23 11:00:27 +0200

    bton añadir

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import{ listProducts } from"./listProducts.js"
-import{insertProducts} from"./insertProducts.js"
+import{ insertProducts } from"./insertProducts.js"
 
 /**
  * Autor: Alejandro González Corroto Fernández
@@ -8,12 +8,20 @@ import{insertProducts} from"./insertProducts.js"
 
 // Listamos los productos 
 
-document.addEventListener("DOMContentLoaded", listProducts); 
+document.addEventListener("DOMContentLoaded", listProducts);
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const btnForm = document.getElementById("product-form-events")
-    btnForm.addEventListener("click",()=>{
-        insertProducts()
-    })
-    
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const btnForm = document.getElementById("product-form-events");
+console.log("el boton funciona")
+    btnForm.addEventListener("click", () => {
+        console.log("el boton funciona")
+        
+        insertProducts();
+        
+
+    });
+
+   
+
 });
